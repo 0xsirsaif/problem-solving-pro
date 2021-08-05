cc0945dedc9e04bb6363d0fc578152b6e90859a6
sir saif <sirsaif99@gmail.com>
2021-08-05 10:27:35 +0200

    Review step

--- data_structures_and_algorithms_in_python/ch7_linked_lists/R_7_1.py
+++ data_structures_and_algorithms_in_python/ch7_linked_lists/R_7_1.py
@@ -1,6 +1,16 @@
 """
 Give an algorithm for finding the second-to-last node in a singly linked
 list in which the last node is indicated by a next reference of None.
+
+Algorithm description:
+Moving the ptr from the head to the outer of the list (None = tail.next), and only move the prev if ptr is not None
+
+Notes and review:
+- Dummy Node always come in handy
+- while ptr >> while ptr.next
+- think after ptr.next! Do tou need conditions?
+- explicit return always win, return prev.element >> return prev.next.element, or so
+- single return >> double return
 """
 from data_structures_implementation import SinglyLinkedList, DummyNode
 import unittest
@@ -18,26 +28,31 @@ def find_second_to_last(head):
 
 class TestFindSecondToLast(unittest.TestCase):
     def test_find_second_to_last_empty_list(self):
+        # [] -> the result is None
         s = SinglyLinkedList()
         actual = find_second_to_last(s.head)
         expected = None
         self.assertEqual(actual, expected)
 
     def test_find_second_to_last_one_element(self):
+        # [1] -> the result is None
         s = SinglyLinkedList()
-        s.add_first(10)
+        s.add_first(1)
         actual = find_second_to_last(s.head)
         expected = None
         self.assertEqual(actual, expected)
 
     def test_find_second_to_last_two_elements(self):
+        # [1, 2] -> the result is 1
         s = SinglyLinkedList()
-        s.add_first(10)
+        s.add_first(2)
+        s.add_first(1)
         actual = find_second_to_last(s.head)
-        expected = None
+        expected = 1
         self.assertEqual(actual, expected)
 
     def test_find_second_to_last_multi_elements(self):
+        # [9, 8, 7, 6, 5, 4, 3, 2, 1, 0] -> the result is 1
         s = SinglyLinkedList()
         for i in range(10):
             s.add_first(i)
