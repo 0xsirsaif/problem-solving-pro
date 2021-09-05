cf3cd3c8764f40149007257fe7ad5bf39e5bf4f3
Mohamed Saif <sirsaif99@gmail.com>
2021-09-05 15:39:39 +0200

    RAS Algorithm training, day 1, task 2

---
+++  RAS_Algorithm_Training/day_1/204_count_primes.py
@@ -0,0 +1,16 @@
+"""
+Count the number of prime numbers less than a non-negative number, n.
+
+TODO
+    COMPLETE
+    REVIEW
+"""
+
+
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        pass
+
+
+S = Solution()
+print(S.countPrimes(10))
