c7d0c5d6e84394c508c733d8995ce791b40fa60c
Mohamed Saif <sirsaif99@gmail.com>
2021-09-11 17:37:20 +0200

    rename directories

--- IEEEXtreme15-Training/task_3_bfs_dfs/areas.py
+++
@@ -1,7 +0,0 @@
-"""
-Hacker earth
-
-You are given a 2D grid. A '#' represents an obstacle and a '.' represents free space.
-You need to find the areas of the disconnected components.
-The cells (i+1, j), (i, j+1), (i-1, j), (i, j-1) are the adjacent to the cell (i, j).
-"""
